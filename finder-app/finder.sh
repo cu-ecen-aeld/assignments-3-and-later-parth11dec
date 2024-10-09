cad0c6ff0db2657ccabd184ed275637dbe904d97
Parth M <parth11dec@gmail.com>
2024-10-09 14:46:51 -0700

    Assignment-3 changes - boot to shell successful
    exiting from the shell still throws error

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check for required arguments
 if [ $# -ne 2 ]; then
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +54,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +67,57 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE="$CROSS_COMPILE"
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" install
+chmod u+s ${OUTDIR}/rootfs/bin/busybox
 
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# Create a shell variable for sysroot path
+export SYSROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot)
+cd ${OUTDIR}/rootfs
+cp $SYSROOT/lib/ld-linux-aarch64.so.1 lib
+cp $SYSROOT/lib64/libm.so.6 lib64
+cp $SYSROOT/lib64/libresolv.so.2 lib64
+cp $SYSROOT/lib64/libc.so.6 lib64
 
 # TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+#sudo mknod -m 666 dev/null c 1 3
+#sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ~/linux-system-programming/assignment-2-parth11dec/finder-app/
+make clean && make
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cd ${OUTDIR}/rootfs/home
+cp -r ~/linux-system-programming/assignment-2-parth11dec/finder-app/writer .
+cp -r ~/linux-system-programming/assignment-2-parth11dec/finder-app/finder.sh .
+cp -r ~/linux-system-programming/assignment-2-parth11dec/finder-app/finder-test.sh .
+mkdir conf
+cp -r ~/linux-system-programming/assignment-2-parth11dec/finder-app/conf/username.txt conf/.
+cp -r ~/linux-system-programming/assignment-2-parth11dec/finder-app/conf/assignment.txt conf/.
+cp -r ~/linux-system-programming/assignment-2-parth11dec/finder-app/autorun-qemu.sh .
 
 # TODO: Chown the root directory
+#This step is taken care of by passing --owner root:root when creating a cpio
+#sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ..
+gzip initramfs.cpio
